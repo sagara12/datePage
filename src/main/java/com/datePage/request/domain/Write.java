a385dc71d9d9a9d246f9c51fb59928e423826f5f
sagara12 <sagara12@naver.com>
2022-08-15 12:38:45 +0900

    2020-07-29 로그인 기능 예외 처리 및 테스트 케이스 작성 2020-07-30 게시글 작성 및 조회 테스트 케이스 작성 2020-07-31 응답 클래스 분리 2020-08-01 조회 API 생성 (H2 데이터베이스 연결 문제 해결 못함) 2020-08-02 H2 데이터베이스 연결 문제 해결 및 최적화 2020-08-02 페이징 처리 2020-08-02 Queary DSL을 통한 페티징 처리 2020-08-04 Testcase 문제 해결중 2020-08-04 게시글 수정 구현 2022-8-11 게시글 삭제 2020-08-15 테스트 오류 수정

--- src/main/java/com/datePage/request/domain/Write.java
+++ src/main/java/com/datePage/request/domain/Write.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 public class Write {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
     private Long writeId;
 
