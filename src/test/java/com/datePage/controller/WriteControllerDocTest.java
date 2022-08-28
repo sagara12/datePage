a0e0534c30a22f8cecff809a8e385e6ee4de8ee3
sagara12 <sagara12@naver.com>
2022-08-29 00:10:01 +0900

    2022-07-29 로그인 기능 예외 처리 및 테스트 케이스 작성 2022-07-30 게시글 작성 및 조회 테스트 케이스 작성 2022-07-31 응답 클래스 분리 2022-08-01 조회 API 생성 (H2 데이터베이스 연결 문제 해결 못함) 2022-08-02 H2 데이터베이스 연결 문제 해결 및 최적화 2022-08-02 페이징 처리 2022-08-02 Queary DSL을 통한 페티징 처리 2022-08-04 Testcase 문제 해결중 2022-08-04 게시글 수정 구현 2022-8-11 게시글 삭제 2022-08-15 테스트 오류 수정 2022-08-20 예외 처리 추가 2022-08-20 Test 오류 문제 해결 2022-08-21 컨트롤러 예외 처리 추가 2022-08-29 doc 문서 자동 생성 기능

---
+++ src/test/java/com/datePage/controller/WriteControllerDocTest.java
@@ -0,0 +1,58 @@
+package com.datePage.controller;
+
+
+import com.datePage.repository.WriteRepository;
+import com.datePage.request.domain.Write;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ExtendWith(RestDocumentationExtension.class)
+public class WriteControllerDocTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WriteRepository writeRepository;
+
+    @BeforeEach
+    public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
+
+    @Test
+    @DisplayName("글 단건 조회 테스트")
+    void test1() throws Exception {
+
+        //given
+        Write write = Write.builder()
+                .title("제목")
+                .content("내용")
+                .build();
+
+        writeRepository.save(write);
+
+        this.mockMvc.perform(get("/write/{writeIndex}", 1L).accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andDo(document("index"));
+    }
+}
